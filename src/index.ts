eb84e4b5d10f093ccba423f956fc849bde3f64a0
LoTwT <709937065@qq.com>
2023-08-29 15:27:06 +0800

    feat: prettier config

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-export const one = 1
+import { Config } from "prettier"
+import SxzzConfig from "@sxzz/prettier-config"
+
+const config: Config = {
+  useTabs: false,
+  tabWidth: 2,
+  printWidth: 80,
+  singleQuote: false,
+  trailingComma: "all",
+  semi: false,
+  endOfLine: "lf",
+  overrides: SxzzConfig?.overrides ?? [],
+}
+
+export default config
