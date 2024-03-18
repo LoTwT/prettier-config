19c0c14fd87f7fc818bf0224b6bf6e2b1df1bb80
Lo <l709937065@gmail.com>
2024-03-18 09:26:15 +0800

    fix: dist types export

--- packages/prettier-config/tsup.config.ts
+++ packages/prettier-config/tsup.config.ts
@@ -3,11 +3,9 @@ import { defineConfig } from "tsup"
 export default defineConfig({
   entry: ["index.ts", "config.ts"],
   format: ["esm", "cjs"],
-  clean: true,
   target: "esnext",
-  dts: true,
+  clean: true,
   cjsInterop: true,
-  // https://github.com/egoist/tsup/commit/9b813620a884f5b780346a240c51ead321227bce
+  dts: true,
   splitting: true,
-  platform: "neutral",
 })
